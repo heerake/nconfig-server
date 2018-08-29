ee0eebe8f452290f75daf7304e081b3bb997cc30
杨旸 <yangyang@qingting.fm>
2018-08-29 19:21:44 +0800

    change init

--- src/register/register.ts
+++ src/register/register.ts
@@ -18,11 +18,29 @@ export default function (app: Koa) {
       env = data.env
       clientManager.addClient(socket, areaID, env)
 
-      socket.emit('getalldata', areaManager.getArea(areaID).getEnv(env))
+      let kvObj = Object.values(areaManager.getArea(areaID).getEnv(env)).reduce((o: any, t) => {
+        o[t.key] = t.get()
+        return o
+      }, {})
+
+      socket.emit('initdata', kvObj)
     })
 
     socket.on('disconnect', () => {
       clientManager.removeClient(socket, areaID, env)
+    })
+
+    socket.on('get', key => {
+      return areaManager.get(areaID, env, key)
+    })
+
+    socket.on('getalldata', () => {
+      let kvObj = Object.values(areaManager.getArea(areaID).getEnv(env)).reduce((o: any, t) => {
+        o[t.key] = t.get()
+        return o
+      }, {})
+
+      return kvObj
     })
   })
 
